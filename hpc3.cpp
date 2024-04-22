6de09cfc46dd25bb79d1d09c116aeeb44bf1dacd
Lalit Pagar <94441455+lalitpagar03@users.noreply.github.com>
2024-04-22 22:30:08 +0530

    Update hpc3.cpp

--- hpc3.cpp
+++ hpc3.cpp
@@ -1,53 +1,48 @@
 #include<iostream>
 #include<omp.h>
+
 using namespace std;
-int minval(int arr[] , int n){
-int minval = arr[0];
-#pragma omp parallel for reduction(min : minval)
-for(int i = 0 ; i<n ; i++)
-{
-if(arr[i] < minval) {
-minval = arr[i];
-}
-}
-return minval;
-}
-int maxval(int arr[] , int n){
-int maxval = arr[0];
-#pragma omp parallel for reduction(max : maxval)
-for(int i = 0 ; i<n ; i++)
-{
-if(arr[i] > maxval) {
-maxval = arr[i];
-}
-}
-return maxval;
+
+int minval(int arr[], int n) {
+    int minval = arr[0];
+    #pragma omp parallel for reduction(min: minval)
+    for(int i = 0; i < n; i++) {
+        if(arr[i] < minval) {
+            minval = arr[i];
+        }
+    }
+    return minval;
 }
 
+int maxval(int arr[], int n) {
+    int maxval = arr[0];
+    #pragma omp parallel for reduction(max: maxval)
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > maxval) {
+            maxval = arr[i];
+        }
+    }
+    return maxval;
+}
 
-int sum1 (int arr[] , int n)
-{
-int sum = 0;
-#pragma omp parallel for reduction(+: sum)
-for(int i = 0 ; i<n ; i++)
-{
-sum += arr[i];
+int sum1(int arr[], int n) {
+    int sum = 0;
+    #pragma omp parallel for reduction(+: sum)
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return sum;
 }
-return sum;
 
+double average(int arr[], int n) {
+    return (double)sum1(arr, n) / n;
 }
-double average(int arr[] , int n)
-{
-return (double)sum1(arr,n) /n;
-}
-int main()
-{
-int n = 5;
-int arr[] = {1,2,3,4,5};
-cout<<"minimum value: " <<minval(arr,n) << endl;
-cout<<"maximum value: " <<maxval(arr,n) << endl;
-cout<<"sum is: " <<  sum1(arr,n) << endl;
-cout<<"Average is: "<<average(arr,n) << endl;
 
-return 0;
-}
+int main() {
+    int n = 5;
+    int arr[] = {1, 2, 3, 4, 5};
+    
+    cout << "Minimum value: " << minval(arr, n) << endl;
+    cout << "Maximum value: " << maxval(arr, n) << endl;
+    cout << "Sum is: " << sum1(arr, n) << endl;
+    cout << "Average is: " << average
